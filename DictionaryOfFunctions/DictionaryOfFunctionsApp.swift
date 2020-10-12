8b3ba288da9950d068d1cf2ed9e5bfecc2dd99f4
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2020-10-11 21:41:53 -0400

    Initial Commit

---
+++ DictionaryOfFunctions/DictionaryOfFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryOfFunctionsApp.swift
+//  DictionaryOfFunctions
+//
+//  Created by Larry Mcdowell on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryOfFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
